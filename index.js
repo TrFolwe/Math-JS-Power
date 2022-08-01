6cb8f7cdcb9b4e3fa629e3a02bb1073ad35fe343
Semih <78105136+TrFolwe@users.noreply.github.com>
2022-08-01 04:12:58 +0300

    completely renewed

--- index.js
+++ index.js
@@ -1,72 +1,100 @@
 const canvas = document.querySelector("canvas");
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const DARK_BACKGROUND__ = false;
 const COLORFUL_MODE__ = true;
 
 if(DARK_BACKGROUND__) document.getElementsByTagName("body")[0].style.backgroundColor = "black";
 
-let startPosition = {
+const startPosition = {
     x: canvas.width/2,
     y: canvas.height/2
-}
+};
 
-let hexColors = [
-    "#d6def9",
-    "#dee8fd",
-    "#e7f4f3",
-    "#fbf8dc",
-    "#fbfaeb",
-    "#e0ded8",
-    "#e3c5d2",
-    "#d5a5be",
-    "#c1e2e5",
-    "#85cddb"
-];
-
-function circleDrawF(x, y) {
-    const RANDOM_COLORS = "#"+Math.floor(Math.random()*16777215).toString(16);
+function circleDrawF(x, y, color, radius) {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = COLORFUL_MODE__ ? RANDOM_COLORS : hexColors[Math.floor(Math.random() * hexColors.length)];
+    ctx.arc(x, y, radius, 0, Math.PI*2);
+    ctx.fillStyle = color;
     ctx.fill();
     ctx.closePath();
 }
 
-function drawRect(x, y) {
-    const RANDOM_COLORS = "#"+Math.floor(Math.random()*16777215).toString(16);
+function drawRect(x, y, width, height, color) {
     ctx.beginPath();
-    ctx.fillStyle = COLORFUL_MODE__ ? RANDOM_COLORS : hexColors[Math.floor(Math.random() * hexColors.length)];
-    ctx.fillRect(x, y, 25, 25);
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
     ctx.closePath();
 }
 
-let options = ["circle","cube"];
-let selectedOption = options[0];
-
-let Ball = {
-    rad: 0,
-    RADIUS: 3,
-    speed: 25,
-    RadiusBlob: 1,
-    maxRadius: 800
+const objectOptions = {
+    "object": "circle",
+    "randomObject": true,
+    "circle": {
+        "initialRadian": 0,
+        "initialRadius": 3,
+        "minScale": 10,
+        "maxScale": 10,
+        "movementRadius": 1,
+        "maxRadius": 800,
+        "speed": 25,
+        "color": "RANDOM"
+    },
+    "cube": {
+        "initialRadian": 0,
+        "initialRadius": 3,
+        "minScale": 25,
+        "maxScale": 25,
+        "movementRadius": 1,
+        "maxRadius": 800,
+        "speed": 25,
+        "color": "RANDOM"
+    }
 };
 
-function reset() {
+const myObject = {
+    "radian": objectOptions[objectOptions.object].initialRadian,
+    "radius": objectOptions[objectOptions.object].initialRadius,
+};
+
+function resetObjects() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    Ball.rad = 0;
-    Ball.RADIUS = 0;
-    let randomMode = false;
-    if(randomMode) selectedOption = selectedOption === "circle" ? options[1] : options[0];
+    if(objectOptions.randomObject) objectOptions.object = objectOptions.object === "circle" ? "cube" : "circle";
+    myObject.radian = objectOptions[objectOptions.object].initialRadian;
+    myObject.radius = objectOptions[objectOptions.object].initialRadius;
 }
 
-function render() {    
-    if(Ball.RADIUS > Ball.maxRadius) reset();
-    selectedOption ==="circle" ? circleDrawF(Math.cos(Ball.rad)*Ball.RADIUS+startPosition.x, Math.sin(Ball.rad)*Ball.RADIUS+startPosition.y) : drawRect(Math.cos(Ball.rad)*Ball.RADIUS+startPosition.x, Math.sin(Ball.rad)*Ball.RADIUS+startPosition.y);
-    Ball.rad += (Math.PI/180)*Ball.speed;
-    Ball.RADIUS+=Ball.RadiusBlob;
+function render() {
+    if(!Object.keys(objectOptions).includes(objectOptions.object)) {
+        console.error("Object type not found!!!");
+        return;
+    }
+    let posX = Math.cos(myObject.radian) * myObject.radius + startPosition.x;
+    let posY = Math.sin(myObject.radian) * myObject.radius + startPosition.y;
+    let selectedObject = objectOptions[objectOptions.object];
+    let color = selectedObject.color === "RANDOM" ? "#" + Math.floor(Math.random()*16777215).toString(16) : selectedObject.color;
+    let scale = random(selectedObject.minScale, selectedObject.maxScale);
+
+    if(myObject.radius > selectedObject.maxRadius) {
+        resetObjects();
+    }
+
+    objectOptions.object === "circle" ? circleDrawF(
+        posX,
+        posY,
+        color,
+        scale
+    ) : drawRect(
+        posX,
+        posY,
+        scale,
+        scale,
+        color
+    );
+    myObject.radian += (Math.PI / 180) * selectedObject.speed;
+    myObject.radius += selectedObject.movementRadius;
     requestAnimationFrame(render);
 }
 render();
